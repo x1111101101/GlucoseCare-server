84f830fb905c953ffb6e276224354c4a3d7beaed
Sangyeob <x1111101101@gmail.com>
2024-08-26 21:23:56 +0900

    IntakeRecord totalNutrition

--- src/main/kotlin/io/github/x1111101101/glucoseserver/record/model/intake/IntakeRecord.kt
+++ src/main/kotlin/io/github/x1111101101/glucoseserver/record/model/intake/IntakeRecord.kt
@@ -1,19 +1,21 @@
 package io.github.x1111101101.glucoseserver.record.model.intake
 
+import io.github.x1111101101.glucoseserver.food.dish.model.api.nutrition.Nutrition
 import io.github.x1111101101.glucoseserver.record.model.Record
 import io.github.x1111101101.glucoseserver.record.model.RecordType
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
 @Serializable
 data class IntakeRecord(
     override val date: Int,
     override val deleted: Boolean = false,
-    override val uuid: String,
+    override val uuid: String = UUID.randomUUID().toString(),
     override val version: Long,
     override val time: Int,
     val intakeType: IntakeType,
-    val items: List<IntakeItem>
-
+    val items: List<IntakeItem> = emptyList(),
+    val totalNutrition: Nutrition
 ): Record {
     override val type: RecordType = RecordType.INTAKE
 }
